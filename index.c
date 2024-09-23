6db3656f9084ac1e20457ad3e67243cf363ad50e
yassinedesu <yassydesuwork@gmail.com>
2024-09-23 17:43:36 +0200

    Update index.c

--- index.c
+++ index.c
@@ -5,8 +5,8 @@ int main() {
     // Write C code here
     char chaine[6] = {'h','e','l','l','o','/0};
     printf("%c", chaine[2]);
-    
-    
 
+    // feel free to modify the code right here
+    
     return 0;
 }
