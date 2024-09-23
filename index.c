179b2b6d9a3812ed022aa279b391cfd4f64f697c
mohammed-hannan <mohammedhannane@gmail.com>
2024-09-23 15:24:48 +0100

    first code

---
+++ index.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char chaine[6] = {'h','e','l','l','o','0};
+    printf("%c", chaine[2]);
+    
+    
+
+    return 0;
+}
